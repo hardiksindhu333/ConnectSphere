5ef0a2084d5bb92b2dec79edec987c839c63bec5
hardiksindhu333 <hardiksindhu111@gmail.com>
2026-03-23 00:39:08 +0530

    more func in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,11 +1,8 @@
-import { User } from "../models/user.model";
 import mongoose from "mongoose";
 import { Comment } from "../models/comment.model";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
-import { Video } from "../models/video.model";
-import { use } from "react";
 
 const addComment = asyncHandler(async(req,res) =>{
 // Extract videoId and content, validate both.
@@ -34,23 +31,57 @@ const addComment = asyncHandler(async(req,res) =>{
 
 
 const getVideoComments = asyncHandler(async(req,res) =>{
-
-})
-
-// getVideoComments
 // Extract videoId.
 // Match comments where video = videoId.
 // Lookup owner (user details).
 // Unwind owner.
 // Project required fields.
 // Return comments list.
+    const {videoId} = req.params
+
+    if(!videoId){
+        throw new ApiError(400,"videoId is missing")
+    }
+
+    const comments = await Comment.aggregate([
+        {
+            $match:{
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner"
+            }
+        },
+        {
+            $unwind:"$owner"
+        },
+        {
+            $project:{
+                _id:1,
+                content:1,
+                createdAt:1,
+                owner: {
+                    _id: "$owner._id",
+                    username: "$owner.username",
+                    avatar: "$owner.avatar"
+                }
+            }
+        }
+    ])
+
+    return res.status(200)
+    .json(new ApiResponse(200,comments,"fetched comments successfully"))
+})
+
+
 
 
 const updateComment = asyncHandler(async(req,res) =>{
-    
-})
-
-// 🔹 updateComment
 // Extract commentId and content.
 // Validate inputs.
 // Find comment by ID.
@@ -58,12 +89,36 @@ const updateComment = asyncHandler(async(req,res) =>{
 // Update content.
 // Return updated comment.
 
+    const {commentId} = req.params
+    const {content} = req.body
 
-const deleteComment = asyncHandler(async(req,res) =>{
-    
+    const userId = req.user?._id
+
+    if(!commentId || !content?.trim()){
+        throw new ApiError(400,"invalid data")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(404,"comment not found")
+    }
+
+    if(comment.owner.toString() !== userId.toString()){
+        throw new ApiError(403,"unauthorized")
+    }
+
+    comment.content = content
+    await comment.save()
+
+    return res.status(200)
+    .json(new ApiResponse(200,comment,"updated comment successfully"))
 })
 
-// deleteComment
+
+
+
+const deleteComment = asyncHandler(async(req,res) =>{
 // Extract commentId.
 // Validate input.
 // Find comment.
@@ -71,6 +126,35 @@ const deleteComment = asyncHandler(async(req,res) =>{
 // Delete comment.
 // Return success response.
 
+    const {commentId} = req.params
+
+    const userId = req.user?._id
+
+    if(!commentId){
+        throw new ApiError(404,"comment not found")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(404,"comment not found")
+    }
+
+    if(comment.owner.toString() !== userId.toString()){
+        throw new ApiError(403,"unauthorized")
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+
+    return res.status(200).json(new ApiResponse(200,{},"comment deleted successfully"))
+})
 
 
-export {addComment}
+
+
+
+export {addComment,
+    getVideoComments,
+    updateComment,
+    deleteComment
+}
