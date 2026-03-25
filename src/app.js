52b96c23c46698a2a302d1225ed8960760efcf4d
hardiksindhu333 <hardiksindhu111@gmail.com>
2026-03-26 00:47:47 +0530

    added dashboard route

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
 
 
 
@@ -40,6 +42,9 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use("/api/v1/comments", commentRouter);
+
+app.use("/api/v1/dashboard", dashboardRouter);
+
 
 
 app.use(errorHandler);
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { getChannelStats,
+    getChannelVideos
+ } from "../controllers/dashboard.controller.ks";
 
 const router = Router()
+
+router.get("/stats", verifyJWT, getChannelStats);
+router.get("/videos", verifyJWT, getChannelVideos);
 
 export default router
